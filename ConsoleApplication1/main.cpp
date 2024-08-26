02290722c34e426f136b4dc8728ae43fa75bba59
M S <mdot2x3@gmail.com>
2024-08-26 13:34:04 -0700

    feat: Add to main.cpp
    added required studentData array and initial program output strings.
    
    fixed error relating to #include quotes.

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
-#include “roster.h”
+#include "roster.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    const string studentData[] =
+    { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Max,S,sample_email@wgu.edu,38,7,14,21,SOFTWARE" };
+
+    cout << "C867-Scripting & Programming: Applications" << endl;
+    cout << "Language: C++" << endl;
+    cout << "Student ID: 011092415" << endl;
+    cout << "Name: Max S" << endl;
+
+
 }
